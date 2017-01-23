5d74399410a6245989ffbf0ab8f19e5e185bfb49
Manoel Estevam <atlantisnet@outlook.com.br>
2017-01-22 23:17:58 -0300

    Implementado login avançado

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
-      # Registra o usuário e redirecione para a página de exibição do usuário.
     else
-      flash.now[:danger] = 'Combinação de email/senha inválido' # Não é o certo
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
